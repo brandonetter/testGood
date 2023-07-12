22fd53b2621f8bbb84ecac15fce8cad100f57d58
Brandon Etter <brandonetter@gmail.com>
2023-07-12 17:13:32 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Component />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
